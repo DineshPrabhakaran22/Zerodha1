52985eb2bd3579fa35a0ec422c7fd2317a0eb8e8
Dinesh Prabhakaran <dineshprabhakaran17@gmail.com>
2025-10-23 07:11:23 +0200

    "urls"

--- backend/index.js
+++ backend/index.js
@@ -29,27 +29,26 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,POST,PUT,DELETE,OPTIONS"
+    );
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Content-Type, Authorization, X-Requested-With"
     );
   }
 
-  // Handle preflight request
   if (req.method === "OPTIONS") {
-    return res.sendStatus(204); // No Content
+    return res.sendStatus(204);
   }
 
   next();
 });
 
-
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.json());
-
-
 
 app.get("/allHoldings", async (req, res) => {
   let allHoldings = await HoldingsModel.find({});
